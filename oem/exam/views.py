baff0d4366315c13a171b6adfa73254475b55d20
kushagra <kushagra@localhost.localdomain>
2016-03-07 18:26:36 +0000

    Added post test completion page showing test result

--- oem/exam/views.py
+++ oem/exam/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, render_to_response
 from django.shortcuts import get_object_or_404
-from .models import Test
+from .models import Test, TestResult
 
 # Create your views here.
 def give_test(request, test_id):
@@ -25,3 +25,33 @@ def give_test(request, test_id):
     choice_array.pop()
 
     return render_to_response("test.html", {"question_text" : question_text, "choice_array" : choice_array})
+
+
+def submit_test(request, test_id):
+
+    test_data = Test.objects.get(pk = test_id)
+    question_list = test_data.question_set.all()
+    correct_count = 0
+    marks = 0
+
+    for question in question_list:      # looping over all questions
+        choices = question.choice_set.all()
+
+        correct_index = 0
+        for choice in choices:          # looping over all choices
+            if choice.is_correct:
+                break
+            else:
+                correct_index += 1
+
+        # now match if user has selected correct choice
+        if correct_index == request.POST['choice']:
+            marks += question.marks
+            correct_count += 1
+        # end of matching
+
+    # save it to database
+    result = TestResult(test = test_id, student=request.user, marks=marks)
+    result.save()
+
+    return render_to_response("test_submit.html", {"correct_count" : correct_count, "marks" : marks})
