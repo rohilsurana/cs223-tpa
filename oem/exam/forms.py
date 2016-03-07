d6b41d43132fdda8b39e98d6fa5b076c8b399aae
kushagra <kushagra@localhost.localdomain>
2016-03-07 20:46:30 +0000

    Added question text in test page

--- oem/exam/forms.py
+++ oem/exam/forms.py
@@ -9,6 +9,15 @@ class TestForm(forms.Form):
         counter = 1
         for question in questions:
             choice_fields = [(choice.id, str(choice)) for choice in question.choice_set.all()]
-            self.fields['layout']
+
+            #self.fields['layout']
+            self.fields['question_text-' + str(counter)] = forms.CharField(initial=question.question_text, label="")
+            self.fields['question_text-' + str(counter)].disabled = True
+            self.fields['question_text-' + str(counter)].widget.attrs.update({'style' : 'font-size:20; border:none; background-color: white; color:black;'})
+
+            #self.fields['question_text-' + str(counter)]
+
             self.fields['question-' + str(counter)] = forms.ChoiceField(choices=choice_fields, widget=RadioSelect)
+            self.fields['question-' + str(counter)].label = ''
+
             counter += 1
