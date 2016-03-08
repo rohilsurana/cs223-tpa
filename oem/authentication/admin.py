ab49d2578c560edf4c422fd7c3292d90f0cb613b
Rohil Surana <rohilsurana96@gmail.com>
2016-03-09 01:44:42 +0530

    Added a dataset

--- oem/authentication/admin.py
+++ oem/authentication/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.utils.translation import ugettext_lazy
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext as _
 from . import models
 
 # Register your models here.
-
 
 admin.site.register([models.Faculty, models.Student])
 admin.site.site_header = ugettext_lazy('Objective Exam Management')
